63cd3f6fb888177024d00e0cb4ae3153cef777fc
mycaio <caio.mendonca@edu.unifil.br>
2024-11-29 23:46:46 -0300

    subindo projeto final

---
+++ script.js
@@ -0,0 +1,11 @@
+const paragrafo = document.getElementById("sobre-mim");
+
+const texto = `
+Sou aluno de Engenharia de Software na UniFil, tenho 20 anos e sempre fui fascinado por tecnologia. Mas de onde vem esse interesse? Desde pequeno, eu adorava "fuçar" nas coisas. Com apenas 11 anos, já conseguia quebrar o sistema operacional do meu computador (involuntariamente, claro), o que me obrigava a levá-lo para formatar. Naquela época, o custo era alto, e isso me motivou a aprender a formatar meu próprio PC.
+
+Aos 14 anos, dei um passo além: aprendi a montar meu próprio computador. Essa experiência abriu portas para novas possibilidades e, algum tempo depois, tive minha primeira oportunidade de me profissionalizar ao cursar o técnico integrado em Mecatrônica. Foi nesse período que descobri minha paixão pela programação, desenvolvendo projetos com Arduino e programando em C++ para a escola.
+
+Esse interesse crescente pela área da tecnologia e programação me levou a escolher a graduação em Engenharia de Software, onde continuo me aprofundando e evoluindo naquilo que amo.
+`;
+
+paragrafo.innerHTML = texto;
